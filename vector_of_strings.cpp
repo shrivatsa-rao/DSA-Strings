8bf5165a1d170886b82e0a34286988e00044af32
Shrivtsa <68243174+shrivatsa-rao@users.noreply.github.com>
2023-04-28 18:01:02 +0530

    updated vector of strings

--- vector_of_strings.cpp
+++ vector_of_strings.cpp
@@ -7,6 +7,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 
+
 // C++ program to demonstrate
 // array of strings using
 // vector class
@@ -21,14 +22,13 @@ int main()
   // Values can be added here using
   // initializer-list
   // syntax
-  std::vector<std::string> colour{"Blue", "Red",
-                                  "Orange"};
+  std::vector<std::string> colour{"alice and bob love leetcode", "i think so too"};
  
   // Strings can be added at any time
   // with push_back
   
- std::cout << colour.size()<< "\n";//here size will be 3 becuse only three elements
- std::cout << colour[0].size();//bhere it gives size of blue is 4.
+ std::cout << colour.size()<< "\n";//here size will be 2 becuse only 2 large strings are available elements
+ std::cout << colour[0].size();//bhere it gives size of first string is 27
   // Print Strings stored in Vector
 //   for (int i = 0; i < colour.size(); i++)
 //       std::cout << colour.size()<< "\n";
